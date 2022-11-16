4be30f2950d1930977b286108ea8eca7a60f97e4
shinhosuck <shinhosuck1973@gmail.com>
2022-11-16 13:09:10 -0800

    update

--- sample.py
+++ sample.py
@@ -1,4 +1,34 @@
 l = {'a':10, 'b':20}
+# print('{a}, {b}'.format(**l))
 
-print('{a}, {b}'.format(**l))
-# Next Video 20 and 21
+my_list = ['cat', 'dog', 'rat', 'fish']
+
+index = 0
+
+for num in range(0, len(my_list)):
+    # print(num)
+    index = num
+# print('{}'.format(my_list[index]))
+
+def my_func(*args, **kwargs):
+    # print('args:', *args)
+    # print('kwargs:', **kwargs)
+    return
+my_func(my_list)
+my_func('cat', 'dog', 'rat')
+
+a = 1
+b = 'hello'
+data = None
+try:
+    data = my_list[4]
+except:
+    # data = None
+    pass
+
+if data:
+    print(data)
+else:
+    print("no data")
+# print(is_instance)
+# Next Video 21 and 23
