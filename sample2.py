8350ca438e0822329748b3feca3faee9c5fda701
shinhosuck <shinhosuck1973@gmail.com>
2023-03-26 03:14:01 +0800

    update

--- sample2.py
+++ sample2.py
@@ -1,7 +1,13 @@
 
 
-def str_func():
-    return 'hello world'
-str_func()
+def some_func(a, *args, **kwargs):
+    arg = args
+    return kwargs
 
 
+a = "snake"
+b = "elephant"
+c =  b=a
+
+data = some_func(a, b, c)
+print(data)
